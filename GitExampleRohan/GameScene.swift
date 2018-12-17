bf1aee2ba865b3b69d63d17fc076670dec5d068f
Rohan Patil <rohanpatil@tradesocio.com>
2018-12-17 17:57:01 +0530

    change in JFDBroker branch by Rohan

--- GitExampleRohan/GameScene.swift
+++ GitExampleRohan/GameScene.swift
@@ -21,7 +21,7 @@ class GameScene: SKScene {
         addChild(Player1)
         
         let Player2 = SKSpriteNode(imageNamed: "drawdown-2")
-        Player2.setScale(0.5)
+        Player2.setScale(0.7)
         Player2.position = CGPoint(x: 0, y: 100)
         Player2.zPosition = 1
         addChild(Player2)
